defda5295cdffe20d37000534632cdb2437d8937
Sunny <sunnysagar620@gmail.com>
2024-06-27 17:25:38 +0530

    commit init

---
+++ src/main/java/com/sunny/SmartFinTrack/SmartFinTrackApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.SmartFinTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinTrackApplication.class, args);
+	}
+
+}
